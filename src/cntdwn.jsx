cb6f9df3a1d9de1e9be1c72abd6eef91a445d9e3
Sung Won Cho <mikeswcho@gmail.com>
2015-12-12 16:36:56 +1100

    Implement tick with an optional onFinished callback

--- src/cntdwn.jsx
+++ src/cntdwn.jsx
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import moment from 'moment';
 
-export default class Countdown extends React.Component {
+const NOT_STARTED = 1;
+const STARTED = 2;
+const FINISHED = 3;
+
+export default class Countdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remainingTime: 0,
+      status: NOT_STARTED
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.tick();
+      this.setState({status: STARTED});
+
+      this.timer = setInterval(() => {
+        this.tick();
+      }, this.props.interval);
+    }, this.props.startDelay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  calculateRemainingTime() {
+    let now = moment().toDate();
+    return moment(this.props.targetDate).diff(moment(now));
+  }
+
+  tick() {
+    this.setState({remainingTime: this.calculateRemainingTime()});
+
+    if (this.state.remainingTime <= 0) {
+      this.setState({status: FINISHED});
+
+      if (this.props.onFinished) {
+        this.props.onFinished();
+      }
+
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
+    if (this.state.status === NOT_STARTED) {
+      return <span></span>;
+    }
     return (
       <div>
-        Countdown
+        {this.state.remainingTime}
       </div>
     );
   }
 }
+
+Countdown.propTypes = {
+  targetDate: PropTypes.instanceOf(Date).isRequired,
+  interval: PropTypes.number,
+  startDelay: PropTypes.number,
+  onFinished: PropTypes.func
+};
+
+Countdown.defaultProps = {
+  interval: 1000,
+  startDelay: 0
+};
 
 module.exports = Countdown;
